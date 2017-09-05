a41ff2c60e6a5d8e20b3d548acb5c2f45df45c28
Adrien Durand <adrien-durand@live.fr>
2017-09-05 09:20:20 +0200

    tableau presque finit

--- src/Tableau.java
+++ src/Tableau.java
@@ -1,40 +1,27 @@
 
 public class Tableau {
-	protected static char lebonbon=' ';
+	protected static char lebonbon='O';
+	protected static String uneligne="+-----";
+	protected static String unecolonne="|  "+lebonbon+"  ";
+	
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				System.out.println("+-----");
-				if(j==4){
-					System.out.println("+\n");
+		for (int i = 0; i < 13; i++) {
+			for (int j = 0; j < 6; j++) {
+				if(i%2==0){
+					if(j==5){
+						System.out.print("+-----+");
+					}else{
+						System.out.print(uneligne);
+					}
+				}else{
+					if(j==5){
+						System.out.print("|  "+lebonbon+"  |");
+					}else{
+						System.out.print(unecolonne);
+					}
 				}
 			}
-			for (int k = 0; k < 5; k++) {
-				System.out.println("|  "+lebonbon+"  |");
-				if(k==4){
-					System.out.println("+\n");
-				}
-				
-			}
-			for (int l = 0; l < 5; l++) {
-				System.out.println("+-----");
-				if(l==4){
-					System.out.println("+\n");
-				}
-			}
+			System.out.println();
 		}
 	}
-	/*protected static String unecase=" ____\n | "+lebonbon;
-	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			if(i==5){
-				unecase=" ____\n | "+lebonbon+" |\n |___|";
-			}
-			for (int j = 0; j < 5; j++) {
-				System.out.println(unecase);
-			}
-		}
-	}*/
 }
-
-
