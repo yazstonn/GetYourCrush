7ce66a74bc6a5ace3970e55decd9bf5a23bdd491
Amaury TRICQUET <amaury.tricquet@etudiant.univ-lille1.fr>
2017-09-05 10:21:35 +0200

    MODIF : tailles symboles

--- src/BonbonStandard.java
+++ src/BonbonStandard.java
@@ -5,7 +5,7 @@ public class BonbonStandard{
 		typeBonbons[0] = new Bonbons(1,"\u00A4",false,0);
 		typeBonbons[1] = new Bonbons(2,"\u01A2",false,0);
 		typeBonbons[2] = new Bonbons(3,"\u0277",false,0);
-		typeBonbons[3] = new Bonbons(4,"\u03E5",false,0);
+		typeBonbons[3] = new Bonbons(4,"\u047A",false,0);
 	}
 	public Bonbons getBb(int id){
 		return typeBonbons[id-1];
--- src/exemple.java
+++ src/exemple.java
@@ -2,6 +2,6 @@
 public class exemple {
 	public static void main(String[] args) {
 		BonbonStandard bb = new BonbonStandard();
-		System.out.println(bb.getBb(1) + "_" +bb.getBb(2) + "_" + bb.getBb(3)+ "_" + bb.getBb(4)+"\n");
+		System.out.println(bb.getBb(1) + " _ " +bb.getBb(2) + " _ " + bb.getBb(3)+ " _ " + bb.getBb(4)+"\n");
 	}
 }
