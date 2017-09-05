21a0a9d4a837cc72bd2ea43f01361ae531106225
Adrien Durand <adrien-durand@live.fr>
2017-09-05 10:56:37 +0200

    ajout de methodes dans Tableau pour modifier une case et ajout d'un tableau de bonbons

--- src/Tableau.java
+++ src/Tableau.java
@@ -5,9 +5,22 @@ public class Tableau {
 	protected static String uneligne="+-----";
 	protected static String unecolonne="";
 	
+	public static void remplissageBonbons(){
+		Random rnd = new Random();
+		BonbonStandard bb = new BonbonStandard();
+		for(int i=0;i<6;i++){
+			for (int j = 0; j < 6; j++) {
+				bonbons[i][j]=bb.getBb(rnd.nextInt(4)+1);
+			}
+		}
+	}
+	public static void changerBonbon(int ordonnee, int abscisse, Bonbons valeur){
+		bonbons[ordonnee][abscisse]=valeur;
+	}
 	public static void tableauRandom(){
 		Random rnd = new Random();
 		BonbonStandard bb = new BonbonStandard();
+		int k=0;
 		for (int i = 0; i < 13; i++) {
 			for (int j = 0; j < 6; j++) {
 				if(i%2==0){
@@ -18,9 +31,9 @@ public class Tableau {
 					}
 				}else{
 					if(j==5){
-						System.out.print("|  "+bb.getBb(rnd.nextInt(4)+1)+"  |");
+						System.out.print("|  "+bonbons[i/2][j]+"  |");
 					}else{
-						unecolonne="|  "+bb.getBb(rnd.nextInt(4)+1)+"  ";
+						unecolonne="|  "+bonbons[i/2][j]+"  ";
 						System.out.print(unecolonne);
 					}
 				}
@@ -29,25 +42,7 @@ public class Tableau {
 		}
 	}
 	public static void main(String[] args) {
-		/*for (int i = 0; i < 13; i++) {
-			for (int j = 0; j < 6; j++) {
-				if(i%2==0){
-					if(j==5){
-						System.out.print("+-----+");
-					}else{
-						System.out.print(uneligne);
-					}
-				}else{
-					if(j==5){
-						System.out.print("|  "+bonbons[i][j]+"  |");
-					}else{
-						System.out.print("|  "+bonbons[i][j]+"  ");
-					}
-				}
-			}
-			System.out.println();
-		}
-	}*/
+		remplissageBonbons();
 		tableauRandom();
 	}
 }
