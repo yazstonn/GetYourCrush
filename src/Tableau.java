bbd6c3f517bd05824352095ec52528f209b5cab8
Florian LESTIENNE <florian.lestienne@etudiant.univ-lille1.fr>
2017-09-05 14:43:12 +0200

    ADD : methode possibiliteRestantes

--- src/Tableau.java
+++ src/Tableau.java
@@ -1,39 +1,49 @@
 import java.util.Random;
 
 public class Tableau {
-	protected static Bonbons[][] bonbons = new Bonbons[6][6];
-	protected static String uneligne="+-----";
-	protected static String unecolonne="";
-	
-	public static void remplissageBonbons(){
+	private Bonbons[][] bonbons = new Bonbons[6][6];
+	protected String uneligne = "+-----";
+	protected String unecolonne = "";
+
+	public Tableau() {
+		remplissageBonbons();
+	}
+
+	public Bonbons[][] getTab() {
+		return bonbons;
+	}
+
+	public void remplissageBonbons() {
 		Random rnd = new Random();
 		BonbonStandard bb = new BonbonStandard();
-		for(int i=0;i<6;i++){
+		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 6; j++) {
-				bonbons[i][j]=bb.getBb(rnd.nextInt(4)+1);
+				bonbons[i][j] = bb.getBb(rnd.nextInt(4) + 1);
 			}
 		}
 	}
-	public static void setBonbon(int ordonnee, int abscisse, Bonbons valeur){
-		bonbons[ordonnee][abscisse]=valeur;
+
+	public void setBonbon(int ordonnee, int abscisse, Bonbons valeur) {
+		bonbons[ordonnee][abscisse] = valeur;
 	}
-	public static void afficheTableau(){
+
+	public void afficheTableau() {
 		Random rnd = new Random();
 		BonbonStandard bb = new BonbonStandard();
-		int k=0;
+		int k = 0;
 		for (int i = 0; i < 13; i++) {
 			for (int j = 0; j < 6; j++) {
-				if(i%2==0){
-					if(j==5){
+				if (i % 2 == 0) {
+					if (j == 5) {
 						System.out.print("+-----+");
-					}else{
+					} else {
 						System.out.print(uneligne);
 					}
-				}else{
-					if(j==5){
-						System.out.print("|  "+bonbons[i/2][j]+"  |");
-					}else{
-						unecolonne="|  "+bonbons[i/2][j]+"  ";
+				} else {
+					if (j == 5) {
+						System.out.print("|  " + bonbons[i / 2][j] + "  |");
+					} else {
+						unecolonne = "|  " + bonbons[i / 2][j] + "  ";
 						System.out.print(unecolonne);
 					}
 				}
@@ -41,22 +51,71 @@ public class Tableau {
 			System.out.println();
 		}
 	}
-	
-	public static Bonbons getBonbons(int ordonnee, int abscisse){
+
+	public Bonbons getBonbons(int ordonnee, int abscisse) {
 		return bonbons[ordonnee][abscisse];
 	}
-	
-	public boolean decomptePossibilite(){
-		return true;
-//		int cpt = 0;
-//		for(int i = 0; i<bonbons.length; i++){
-//			
-//		}
-		
+
+	private boolean verifHorizontal() {
+		Bonbons tmp = null;
+		boolean verif = false;
+		int cpt = 1;
+
+		for (int i = 0; i < bonbons.length; i++) {
+			cpt = 1;
+			for (int j = 1; j < bonbons[0].length; j++) {
+				tmp = bonbons[i][j - 1];
+				if (tmp.getId() == bonbons[i][j].getId()) {
+					cpt++;
+				} else {
+					cpt = 1;
+				}
+				if (cpt >= 3) {
+					verif = true;
+				}
+			}
+		}
+		return verif;
 	}
-	
-	
-	public static void main(String[] args) {
+
+	private boolean verifVertical() {
+		Bonbons tmp = null;
+		boolean verif = false;
+		int cpt = 1;
+
+		for (int j = 0; j < bonbons.length; j++) {
+			cpt = 1;
+			for (int i = 1; i < bonbons[0].length; i++) {
+				tmp = bonbons[i - 1][j];
+				if (tmp.getId() == bonbons[i][j].getId()) {
+					cpt++;
+				} else {
+					cpt = 1;
+				}
+				if (cpt >= 3) {
+					verif = true;
+				}
+			}
+		}
+		return verif;
+	}
+
+	public boolean decomptePossibilite() {
+		if (verifHorizontal() && verifVertical()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public void siBlocage() {
+		while (!decomptePossibilite()) {
+			remplissageBonbons();
+		}
+	}
+
+	public void main() {
 		remplissageBonbons();
 		afficheTableau();
 	}
---
+++ src/Test.java
@@ -0,0 +1,25 @@
+
+//public class Test {
+//	
+//	public static void main(String [] args){
+//		int cpt = 1;
+//		
+//		Tableau tab = new Tableau();
+//		
+//		for(int i = 0; i<tab.getTab().length; i++){
+//			for(int j = 0; j<tab.getTab()[0].length; j++){
+//				tab.setBonbon(i, j, new BonbonStandard().getBb(cpt%4+1));
+//				cpt++;
+//			}
+//		}
+//		
+//	
+//		tab.afficheTableau();
+//		tab.siBlocage();
+//		
+//		tab.afficheTableau();
+//		
+//		
+//	}
+//
+//}
