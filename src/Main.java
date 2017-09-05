911fe6a01b05a803aa19ff1057034be7573de7c9
Amaury TRICQUET <amaury.tricquet@etudiant.univ-lille1.fr>
2017-09-05 10:33:00 +0200

    ADD : main file avec squelette de methode possibilitees et blocage

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Tableau.tableauRandom();
+		System.out.println("nb possibilitées: "+ Possibilitees(Tableau.bonbons));
+	}
+	public static int Possibilitees(Bonbons[][] bbs){
+		
+		
+		return 42;
+	}
+	public static boolean blocage(Bonbons[][] bbs){
+		return false;
+	}
+}
