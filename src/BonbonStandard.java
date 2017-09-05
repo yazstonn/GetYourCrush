2c042eb71a7e8db0ec3cd36a0dcc4ddab82ce784
Coraline13 <coralia.bodea@gmail.com>
2017-09-05 22:57:56 +0200

    couleurs

--- src/BonbonStandard.java
+++ src/BonbonStandard.java
@@ -1,21 +1,20 @@
-
 public class BonbonStandard{
-	private Bonbons[] typeBonbons = new Bonbons[4];
-	public BonbonStandard(){
-		typeBonbons[0] = new Bonbons(1,"\u00A4",false,20);
-		typeBonbons[1] = new Bonbons(2,"\u01A2",false,20);
-		typeBonbons[2] = new Bonbons(3,"\u0277",false,20);
-		typeBonbons[3] = new Bonbons(4,"\u047A",false,20);
-	}
-	public Bonbons getBb(int id){
-		return typeBonbons[id-1];
-	}
-	public boolean compare(Bonbons bb1,Bonbons bb2){
-		if(bb1.getId() == bb2.getId()){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
+    private Bonbons[] typeBonbons = new Bonbons[4];
+    public BonbonStandard(){
+        typeBonbons[0] = new Bonbons(1,"\u001B[31m\u00A4\u001B[0m",false,0);
+        typeBonbons[1] = new Bonbons(2,"\u001B[32m\u01A2\u001B[0m",false,0);
+        typeBonbons[2] = new Bonbons(3,"\u001B[33m\u0277\u001B[0m",false,0);
+        typeBonbons[3] = new Bonbons(4,"\u001B[36m\u047A\u001B[0m",false,0);
+    }
+    public Bonbons getBb(int id){
+        return typeBonbons[id-1];
+    }
+    public boolean compare(Bonbons bb1,Bonbons bb2){
+        if(bb1.getId() == bb2.getId()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
