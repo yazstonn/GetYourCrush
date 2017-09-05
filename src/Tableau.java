683d916bf87b4fdecddda6f6f444aa22b02e6faa
Adrien Durand <adrien-durand@live.fr>
2017-09-05 10:26:23 +0200

    suppression de tableau_rnd et Tableau remplis aléatoirement avec des bonbons

--- src/Tableau.java
+++ src/Tableau.java
@@ -1,10 +1,35 @@
+import java.util.Random;
 
 public class Tableau {
 	protected static Bonbons[][] bonbons = new Bonbons[6][6];
 	protected static String uneligne="+-----";
+	protected static String unecolonne="";
 	
-	public static void main(String[] args) {
+	public static void tableauRandom(){
+		Random rnd = new Random();
+		BonbonStandard bb = new BonbonStandard();
 		for (int i = 0; i < 13; i++) {
+			for (int j = 0; j < 6; j++) {
+				if(i%2==0){
+					if(j==5){
+						System.out.print("+-----+");
+					}else{
+						System.out.print(uneligne);
+					}
+				}else{
+					if(j==5){
+						System.out.print("|  "+bb.getBb(rnd.nextInt(4)+1)+"  |");
+					}else{
+						unecolonne="|  "+bb.getBb(rnd.nextInt(4)+1)+"  ";
+						System.out.print(unecolonne);
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		/*for (int i = 0; i < 13; i++) {
 			for (int j = 0; j < 6; j++) {
 				if(i%2==0){
 					if(j==5){
@@ -22,5 +47,7 @@ public class Tableau {
 			}
 			System.out.println();
 		}
+	}*/
+		tableauRandom();
 	}
 }
